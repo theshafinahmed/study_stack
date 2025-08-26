26e6be7e9f8aeaebbf3db68f039549e653c9f570
Shafin Ahmed <theshafinahemd2003@gmail.com>
2025-08-26 14:34:15 +0600

    Custom length defined and basic landing page created

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,5 +1,9 @@
 function Landing() {
-    return <div>Landing</div>;
+    return (
+        <main className="w-rscreen h-rscreen flex flex-col items-center justify-center">
+            <button className="btn btn-primary">Get Started</button>
+        </main>
+    );
 }
 
 export default Landing;
